5d4fe81c5a9812c7de4db396de2735a97a92bc21
choijunhee <xcbr87fztz@privaterelay.appleid.com>
2025-11-26 20:09:08 +0900

    Add files via upload

---
+++ korea/module.py
@@ -0,0 +1,5 @@
+def add_numbers(a, b):
+    """
+    두 숫자의 합을 반환하는 간단한 함수.
+    """
+    return a + b
---
+++ korea/setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="programyi",
+    version="0.0.1",
+    packages=find_packages(),
+    description="엄청 간단한 예제 패키지",
+    author="yourname",
+)
